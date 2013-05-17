2c844c24b418d37b7cff0295ec8a900b63a53e5e
tobias <tobi-or-not@gmx.de>
2013-05-17 17:13:07 +0200

    rotation and positioning branches working for one case

--- Uebung5/Assets/Main.cs
+++ Uebung5/Assets/Main.cs
@@ -16,55 +16,68 @@ public class Main : MonoBehaviour {
 	
 	}
 	
-	private void createBranch(Transform parentBranch, float rotation) {
+	private void createBranch(Transform parentBranch, float segmentRotation) {
 		Transform parent;
 		Transform newBranch;
 		
-		if (fuse > 1) {
-			return; 
-		} else {
-			fuse++;
-		}
 		
+		Debug.Log("seg rot param: " + segmentRotation);
 		if(parentBranch == null) {
+			Debug.Log("first");
 			parent = Instantiate(branch) as Transform;
-			//parent.transform.position = new Vector3(0, 0, 0);	
+			float y = parent.transform.FindChild("BranchCube").renderer.bounds.size.y * 0.5F;
+			parent.transform.position = Vector3.up * y;
 			newBranch = parent;
 		} else {
+			Debug.Log("after first");
 			parent = parentBranch;	
 			newBranch = Instantiate(branch) as Transform;
 			
-			float angleZ = 40F;
-			float angleY = rotation;
+			float angleZ = 45F;
+			float angleY = segmentRotation;
+			Debug.Log("rot " + segmentRotation);
 			newBranch.transform.eulerAngles = new Vector3(0, angleY, angleZ);
-			
-			
+						
 			newBranch.transform.localScale = new Vector3(SCALE, SCALE, SCALE);
 			newBranch.transform.position = branchBottom(parent, newBranch);
-			
+
 			newBranch.transform.parent = parent.transform;
+			
+			if (fuse > 2) {
+				return; 
+			} else {
+				fuse++;
+			}
 		}
 		
 		
 		
 		int range = 2; //Random.Range(2, 10);
 		float nextSegment = 360 / range;
-		if (newBranch.localScale.y >= 5) {
-			Debug.Log("loc scale: " + newBranch.localScale.y);
-			for(int i=0; i < range; i++) {
-				createBranch(parent, nextSegment * i);
-			}	
-		}
-		 
+		Debug.Log("loc scale: " + newBranch.FindChild("BranchCube").localScale.y);
+		
+		for(int i=0; i < range; i++) {
+			float rot = nextSegment * i;
+			Debug.Log("for rot " +  rot);
+			createBranch(parent, rot);
+		}	 
 		
 	}
 	
 	private Vector3 branchBottom(Transform parent, Transform newBranch) {
-		float x = parent.position.x;
-		float y = parent.renderer.bounds.size.y + newBranch.renderer.bounds.size.y * 0.5F;
+		Transform child = parent.FindChild("BranchCube");
+		float rotation = newBranch.transform.eulerAngles.z;
+		
+		float hypotenuse = newBranch.FindChild("BranchCube").renderer.bounds.size.y * 0.5F;
+		float deltaX = Mathf.Sin(rotation) * hypotenuse;
+		float deltaY = deltaX / Mathf.Tan(rotation);
+		
+		float x = parent.position.x - deltaX;
+		float y = child.renderer.bounds.size.y + deltaY;
 		float z = parent.position.z;
 		
 		Vector3 vector = new Vector3(x, y, z);
 		return vector;
+		
 	}
 }
