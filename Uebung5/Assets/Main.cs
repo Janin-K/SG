e31d1f176085e259d4b44982273b8330fa02d78e
tobias <tobi-or-not@gmx.de>
2013-05-18 16:00:54 +0200

    child rotation working

--- Uebung5/Assets/Main.cs
+++ Uebung5/Assets/Main.cs
@@ -9,33 +9,6 @@ public class Main : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		CreateBranch(null, 0);
-		
-		/*	
-		Transform parent = Instantiate(branch) as Transform;
-		float y = parent.transform.FindChild("BranchCube").renderer.bounds.size.y * 0.5F;
-		parent.transform.position = Vector3.up * y;
-		
-		Transform firstChild = Instantiate(branch) as Transform;
-		
-		float placementY = parent.transform.FindChild("BranchCube").renderer.bounds.size.y + firstChild.transform.FindChild("BranchCube").renderer.bounds.size.y * 0.5F;
-		Vector3 top = new Vector3(parent.position.x, placementY, parent.position.z);
-		
-		firstChild.transform.position = top;
-		firstChild.transform.parent = parent.transform;
-		
-		float topY = parent.transform.FindChild("BranchCube").renderer.bounds.size.y;
-		Vector3 topVector = parent.transform.position;
-		topVector.y = topY;
-		
-		firstChild.transform.RotateAround(topVector, new Vector3(1, 0, 1), 45);
-		//firstChild.transform.RotateAround(topVector, Vector3.up, 45);
-		
-		Transform secondChild = Instantiate(branch) as Transform;
-		secondChild.transform.parent = firstChild.transform;
-		
-		Debug.Log("parentcount " + NestingDepth(parent));
-		 */
-		
 	}
 	
 
@@ -61,16 +34,16 @@ public class Main : MonoBehaviour {
 			float angleZ = 45F;
 			float angleY = segmentRotation;
 			
-			//newBranch.transform.eulerAngles = new Vector3(0, angleY, angleZ);
 			newBranch.transform.localScale = new Vector3(SCALE, SCALE, SCALE);
 			newBranch.transform.position = ChildPosition(parent, newBranch);
-			newBranch.transform.RotateAround(ParentTop(parent), Vector3.up, segmentRotation);
+			
 			newBranch.transform.RotateAround(ParentTop(parent), Vector3.forward, 45);
+			newBranch.transform.RotateAround(ParentTop(parent), Vector3.up, segmentRotation);
 			newBranch.transform.parent = parent.transform;
 		}
 		
 		if(NestingDepth(newBranch) < MAX_NESTING_DEPTH) {
-			int range = 2; //Random.Range(2, 10);
+			int range = 3; //Random.Range(2, 10);
 			float nextSegment = 360 / range;
 			
 			for(int i=0; i < range; i++) {
