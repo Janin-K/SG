7d023b0b4fe2329245669d8970ddb5e7cfac3261
Janin K <Janin.K@gmx.de>
2013-06-16 23:02:33 +0200

    Agressor as Prefabs done

--- Uebung9/Uebung9/Assets/Player.cs
+++ Uebung9/Uebung9/Assets/Player.cs
@@ -3,20 +3,29 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public Transform player;
+	public PhysicMaterial bounce;
+	public Transform agressor1;
+	public Transform agressor2;
+	public Transform agressor3;
+	
+	private Transform ag1;
 
 	// Use this for initialization
 	void Start () {
-	
+		ag1 = Instantiate(agressor1) as Transform;
+		
+		
+		ag1.transform.rigidbody.AddForce(new Vector3(Random.Range(-100,100),Random.Range(-100,100),0));
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		float deltaX = Input.GetAxis("Mouse X");
 		float deltaY = Input.GetAxis("Mouse Y");
-	   
-	   	//if(deltaX != 0 || deltaY != 0) {
-	    	player.transform.position = new Vector3(deltaY, 0, deltaX);
-	   	//}
+
+	    player.transform.position += new Vector3(deltaX,deltaY,0);
+	 
 		
 	}
 }
